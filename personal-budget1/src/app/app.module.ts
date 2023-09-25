8d7c031c8fecdf2b2a2d4eddf76e17989741003b
Likhita Sree <likhitasree.atla8@gmail.com>
2023-09-24 23:48:29 -0400

    all changes

--- personal-budget1/src/app/app.module.ts
+++ personal-budget1/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,8 @@ import { HttpClientModule } from '@angular/common/http';
     AboutComponent,
     LoginComponent,
     P404Component,
+    BreadcrumbsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
